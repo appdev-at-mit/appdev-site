f0d35655805010fd82b39072d339845254eb3b23
Hailey Pan <haileyrpan@gmail.com>
2025-07-21 14:36:29 -0500

    update role colors

--- src/app/components/rolebadge.tsx
+++ src/app/components/rolebadge.tsx
@@ -6,6 +6,10 @@ const getRoleColor = (role: string): string => {
       return "bg-[#F7BEC2] text-gray-100";
     case "Finance Chair":
       return "bg-[#D2CCED] text-gray-800";
+    case "Webmaster":
+      return "bg-[#e39a62] text-gray-100";
+    case "Team Lead":
+      return "bg-[#edadad] text-gray-800";
     case "Backend Engineer":
       return "bg-appdev-green text-gray-100";
     case "Frontend Engineer":
@@ -22,7 +26,9 @@ export default function RoleBadges({ roles }: { roles: string[] }) {
     <div className="flex flex-wrap gap-2">
       {roles.map((role, index) => (
         <span
-          className={`inline-block px-3 py-1 rounded-md font-mono text-sm font-semibold ${getRoleColor(role)}`}
+          className={`inline-block px-3 py-1 rounded-md font-mono text-sm font-semibold ${getRoleColor(
+            role
+          )}`}
           key={index}
           role={role}
         >
